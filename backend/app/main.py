6185b5b5e15fe04afdeb1c2b227681e9881534e8
Pawan Vishwakarma <pavanvish46@gmail.com>
2026-09-26 16:53:34 +0530

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,11 +24,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://catalyst-key6.onrender.com",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=settings.cors_origin_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
